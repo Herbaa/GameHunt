190d4c052f0ad7495c9b65a3a5f6dd17f0d746bc
Herbaa <142122130+Herbaa@users.noreply.github.com>
2026-04-28 17:24:39 +0400

    disable console.log

--- src/App.jsx
+++ src/App.jsx
@@ -131,7 +131,7 @@ export default function App() {
     setStatusOfGame(null)
     setEnterGames([])
     setSelectedTips([])
-    console.log(secretGame)
+    // console.log(secretGame)
   }
 
   const handleDifficulty = (key) => {
